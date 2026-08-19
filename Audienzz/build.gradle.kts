09fda68a6ee92d5fb38688e9b2f4a43de2741f2a
yevhenii-audienzz <yevhenii.lysiak@audienzz.com>
2026-08-19 17:14:41 +0300

    Bump Android SDK version to 0.1.7

--- Audienzz/build.gradle.kts
+++ Audienzz/build.gradle.kts
@@ -17,7 +17,7 @@ ksp {
     arg(RoomSchemaArgProvider(File(projectDir, "schemas")))
 }
 
-val audienzzSdkVersion = "0.1.5"
+val audienzzSdkVersion = "0.1.7"
 
 android {
     compileSdk = libs.versions.sdk.compile.get().toInt()
