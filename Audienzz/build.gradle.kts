1c76bd30a0415f8963427630c5120e6853f16e9b
elysiak <yevhenii.lysiak@audienzz.com>
2026-09-02 12:26:29 +0300

    Release 0.2.0

--- Audienzz/build.gradle.kts
+++ Audienzz/build.gradle.kts
@@ -17,7 +17,7 @@ ksp {
     arg(RoomSchemaArgProvider(File(projectDir, "schemas")))
 }
 
-val audienzzSdkVersion = "0.1.7"
+val audienzzSdkVersion = "0.2.0"
 
 android {
     compileSdk = libs.versions.sdk.compile.get().toInt()
